c92c07c98f35a96d2e0257eeda4e9b99c6970fe7
monali00c <monalichaki09@gmail.com>
2022-11-03 21:37:56 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,47 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavScrollExample() {
+  return (
+    <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">BLOG</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">HOME</Nav.Link>
+            <Nav.Link href="#action2">ABOUT</Nav.Link>
+              <Nav.Link href="#action3">CONTACT</Nav.Link>
+              <Nav.Link href="#action4">
+                WRITE
+              </Nav.Link>
+              <NavDropdown.Divider />
+              <Nav.Link href="#action5">
+                LOGOUT
+              </Nav.Link>
+            
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
